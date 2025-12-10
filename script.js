0cfc9b63d5401b89b1b045c78dcae597d58f3390
utilisateur2 <utilisateur2@gmail.com>
2025-12-10 09:24:31 +0100

    Ajout de la fonctionnalité d'ajout de taches

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("To-Do App initialisée.");
+function ajouterTache(){
+    const input = document.getElementById("tasKInput");
+    const texte =input.value.trim();
+    
+    if (texte !=="") {
+        const li =document.createElement("li");
+        li.textContent = texte;
+        document.getElementById("tasKList").appendChild(li);
+        input.value = "";
+    }
+
+
+    const boutonAjout = document.getElementById("addTasKBtn");
+    boutonAjout.addEventListener("click",ajouterTache);
+
+}
